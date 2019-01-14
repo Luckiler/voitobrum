db67e58c8e139c82adc3da3390ad393916f68463
Lucas Viegas Milani <luckiler3@gmail.com>
2019-01-14 16:08:12 +0100

    Added screen surface initialisation and test

--- src/v_window.cpp
+++ src/v_window.cpp
@@ -12,4 +12,15 @@ v_window::v_window() {
     if (window == nullptr) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
     }
+    else
+    {
+        this->screenSurface = SDL_GetWindowSurface( this->window );
+
+        // For testing purposes
+        {
+            SDL_FillRect(this->screenSurface, nullptr, SDL_MapRGB(this->screenSurface->format, 0x00, 0xFF, 0x00));
+            SDL_UpdateWindowSurface(this->window);
+            SDL_Delay(2000);
+        }
+    }
 }
