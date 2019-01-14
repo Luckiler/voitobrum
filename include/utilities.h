a3d99d1a9276d65af1a00e8bf9c624e67915f475
Lucas Viegas Milani <luckiler3@gmail.com>
2019-01-14 16:08:45 +0100

    Comment

--- include/utilities.h
+++ include/utilities.h
@@ -8,6 +8,8 @@
 #define DEFAULT_WINDOW_HEIGHT 480
 #define DEFAULT_WINDOW_WIDTH 640
 
+// Status of SDL
+// Automatically initialized on first window creation
 extern bool sdl_initialized;
 
 extern void init_sdl();
