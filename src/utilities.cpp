1194de16567213ee4069d5c29e5dba2a25d71f9a
Lucas Viegas Milani <luckiler3@gmail.com>
2019-01-15 15:10:22 +0100

    Removed redundant SDL init

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -5,13 +5,3 @@
 #include <SDL2/SDL.h>
 #include <cstdio>
 #include "utilities.h"
-
-bool sdl_initialized = false;
-
-void init_sdl() {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    } else {
-        sdl_initialized = true;
-    }
-}
